82313c85f642d90d0281857cd12d9e4fc5820e48
Christobal <geekarist@users.noreply.github.com>
2020-04-19 16:14:40 +0200

    Create find-property-ads

--- src/hello_etaoin/core.clj
+++ src/hello_etaoin/core.clj
@@ -3,7 +3,8 @@
   (:use etaoin.api))
 
 (defn hello []
-  (def driver (firefox {:headless true}))                   ;; here, a Firefox window should appear
+
+  (def driver (firefox {:headless false}))
 
   ;; let's perform a quick Wiki session
   (go driver "https://en.wikipedia.org/")
@@ -32,17 +33,37 @@
   (refresh driver)
   (println "Page title:" (get-title driver))                ;; "Clojure - Wikipedia"
 
-  ;; stops Firefox and HTTP server
-  (quit driver))
+  (quit driver)
+  )
 
 (defn count-houses [location]
-  (def driver (firefox {:headless true}))
+
+  (def driver (firefox {:headless false}))
 
   (go driver "https://www.seloger.com/recherche-avancee.html")
   (wait-has-text driver {:css ".search_panel_footer .count"} "annonces")
 
-  (get-element-inner-html driver {:css ".search_panel_footer .count strong"}))
+  (get-element-inner-html driver {:css ".search_panel_footer .count strong"})
+
+  (quit driver))
+
+(defn find-property-ads [driver location]
+  (doto-wait 1 driver
+             (go "https://www.seloger.com/recherche-avancee.html")
+             (wait-has-text {:css ".search_panel_footer .count"} "annonces")
+             (click {:css ".c-displayPlaces .containerList"})
+             (click {:css ".c-places input[type=\"text\"]"})
+             (fill {:css ".c-places input[type=\"text\"]"} location)
+             (fill-active k/enter)
+             (click {:css ".c-places .slam-aui-tags-close"})
+             (click {:css ".c-places input[type=\"text\"]"})
+             (fill-active k/tab)
+             (fill-active k/enter)
+             (click {:css ".containerRight .txt_rechercher"}))
+  (wait 10))
 
 (comment
   (hello)
-  (count-houses "montigny sur loing"))
+  (count-houses "montigny sur loing")
+  (with-firefox {:headless false} drv (find-property-ads drv "montigny-sur-loing"))
+  )
